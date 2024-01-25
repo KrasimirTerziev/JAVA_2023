87be623f2309b9ab03999b35956c7af06f024fb1
Krasimir Terziev <kdterziev88@gmail.com>
2024-01-25 21:00:49 +0200

    Others

--- FirstProject/beertag/src/main/java/com/company/web/springdemo/controllers/HelloWorldController.java
+++ FirstProject/beertag/src/main/java/com/company/web/springdemo/controllers/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.company.web.springdemo.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,9 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class HelloWorldController {
 
-    @GetMapping("/hello")
-    public String sayHello() {
-        return "Hello World!";
+    @GetMapping("/hello/{name}")
+    public String sayHello(@PathVariable String name) {
+        return String.format("Hello, %s!", name);
     }
 
 }
